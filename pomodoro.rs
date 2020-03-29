b123c8c86377f5485d797d1e81056d9663d5e6dc
FreeFlyFall <ryanjvanslyke@gmail.com>
2020-03-29 00:37:55 -0400

    Change state variables to be parsed as ints in an iterator

--- pomodoro.rs
+++ pomodoro.rs
@@ -1,8 +1,11 @@
 use std::io;
 use std::io::Write; // Import flush
+use std::process::Command;
 
 fn main() {
     println!("Enter times for work, break, and long break in minutes, and the number of iterations before the long break time activates, separated by spaces.\n\ne.g. \"55 5 25 3\" to work for 55 minutes for three cycles with a 5 min break, followed by a cycle with a 25 minute break:\n");
+
+    let (mut work_time, mut break_time, mut extended_time, mut num_iterations): (i32, i32, i32, i32) = (0,0,0,0);
 
     let mut done: bool = true;
     loop {
@@ -34,9 +37,15 @@ fn main() {
             }        // Parse individual strings from input into a vector
         }
         // Check that the inputs can parse as integers
-        for token in &tokens {
+        for (i,token) in tokens.iter().enumerate() {
             match token.parse::<i32>(){
-                Ok(_) => { done = true; }
+                Ok(n) => {
+                    done = true;
+                    if i==0 { work_time = n; }
+                    else if i==1 { break_time = n; }
+                    else if i==2 { extended_time = n; }
+                    else if i==3 { num_iterations = n; }
+                }
                 Err(_e) => {
                     println!("\n{} is not an integer.",token);
                     done = false;
@@ -46,11 +55,15 @@ fn main() {
         }
         if done == false { continue; }
 
-        let (work_time, break_time, extended_time, iteration_num) = (tokens[0],tokens[1],tokens[2],tokens[3]);
-        println!("\nWork: {} mins\nBreak: {} mins\nExtended: {} mins\nIterations: {}\n",work_time, break_time, extended_time, iteration_num);
+
+
+        println!("\nWork: {} mins\nBreak: {} mins\nExtended: {} mins\nIterations: {}\n",work_time, break_time, extended_time, num_iterations);
         break;
     }
 
-    // Begin timer loop
-    // Use Rodio 
+    /*  loop { 
+        let mut child = Command::new("sleep").arg("1").spawn().unwrap();
+        let _result = child.wait().unwrap();
+    }*/
+            // Use Rodio 
 }
